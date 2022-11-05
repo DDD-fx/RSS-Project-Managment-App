e0817a3b198c6a7d2387b4fdab89edc375960732
Iryna <iryna.kolhanova@gmail.com>
2022-11-04 22:17:01 -0400

    feat: implement one  board

--- rss-pm-app/src/app/boards/boards.module.ts
+++ rss-pm-app/src/app/boards/boards.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BoardsRoutingModule } from './boards-routing.module';
 import { BoardComponent } from './components/board/board.component';
 import { BoardsPageComponent } from './pages/boards-page/boards-page.component';
@@ -8,6 +8,8 @@ import { TranslateModule } from '@ngx-translate/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { NewBoardComponent } from './components/new-board/new-board.component';
 
 @NgModule({
@@ -18,8 +20,11 @@ import { NewBoardComponent } from './components/new-board/new-board.component';
     MatFormFieldModule,
     MatInputModule,
     ReactiveFormsModule,
+    FormsModule,
     TranslateModule,
     MatButtonModule,
+    MatCardModule,
+    MatIconModule,
   ],
 })
 export class BoardsModule {}
--- rss-pm-app/src/app/boards/pages/boards-page/boards-page.component.ts
+++ rss-pm-app/src/app/boards/pages/boards-page/boards-page.component.ts
@@ -7,5 +7,96 @@ import { IBoard } from '../../models/boards.models';
   styleUrls: ['./boards-page.component.scss'],
 })
 export class BoardsPageComponent {
-  boards: IBoard[] = [];
+  boards: IBoard[] = [
+    {
+      id: '9a111e19-24ec-43e1-b8c4-13776842b8d5',
+      title: 'Homework tasks',
+      description: 'My board tasks',
+    },
+    {
+      id: '9a111e19-24ec-43e1-b8c4-13776842b8d6',
+      title: 'Homework tasks',
+      description: 'My board tasks',
+    },
+    {
+      id: '9a111e19-24ec-43e1-b8c4-13776842b8d7',
+      title: 'Homework tasks',
+      description: 'My board tasks',
+    },
+    {
+      id: '9a111e19-24ec-43e1-b8c4-13776842b8d8',
+      title: 'Homework tasks',
+      description: 'My board tasks',
+    },
+    {
+      id: '9a111e19-24ec-43e1-b8c4-13776842b8d9',
+      title: 'Homework tasks',
+      description: 'My board tasks',
+    },
+    {
+      id: '9a111e19-24ec-43e1-b8c4-13776842b8d0',
+      title: 'Homework tasks',
+      description: 'My board tasks',
+    },
+    {
+      id: '9a111e19-24ec-43e1-b8c4-13776842b8d5',
+      title: 'Homework tasks',
+      description: 'My board tasks',
+    },
+    {
+      id: '9a111e19-24ec-43e1-b8c4-13776842b8d6',
+      title: 'Homework tasks',
+      description: 'My board tasks',
+    },
+    {
+      id: '9a111e19-24ec-43e1-b8c4-13776842b8d7',
+      title: 'Homework tasks',
+      description: 'My board tasks',
+    },
+    {
+      id: '9a111e19-24ec-43e1-b8c4-13776842b8d8',
+      title: 'Homework tasks',
+      description: 'My board tasks',
+    },
+    {
+      id: '9a111e19-24ec-43e1-b8c4-13776842b8d9',
+      title: 'Homework tasks',
+      description: 'My board tasks',
+    },
+    {
+      id: '9a111e19-24ec-43e1-b8c4-13776842b8d0',
+      title: 'Homework tasks',
+      description: 'My board tasks',
+    },
+    {
+      id: '9a111e19-24ec-43e1-b8c4-13776842b8d5',
+      title: 'Homework tasks',
+      description: 'My board tasks',
+    },
+    {
+      id: '9a111e19-24ec-43e1-b8c4-13776842b8d6',
+      title: 'Homework tasks',
+      description: 'My board tasks',
+    },
+    {
+      id: '9a111e19-24ec-43e1-b8c4-13776842b8d7',
+      title: 'Homework tasks',
+      description: 'My board tasks',
+    },
+    {
+      id: '9a111e19-24ec-43e1-b8c4-13776842b8d8',
+      title: 'Homework tasks',
+      description: 'My board tasks',
+    },
+    {
+      id: '9a111e19-24ec-43e1-b8c4-13776842b8d9',
+      title: 'Homework tasks',
+      description: 'My board tasks',
+    },
+    {
+      id: '9a111e19-24ec-43e1-b8c4-13776842b8d0',
+      title: 'Homework tasks',
+      description: 'My board tasks',
+    },
+  ];
 }
