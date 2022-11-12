86402161a975782636ed2acdfab780af64ed832e
Iryna <iryna.kolhanova@gmail.com>
2022-11-12 00:40:20 -0500

    fix: sorting pipe

--- rss-pm-app/src/app/boards/pipes/sort.pipe.ts
+++ rss-pm-app/src/app/boards/pipes/sort.pipe.ts
@@ -5,27 +5,13 @@ import { ICreateBoardResp } from 'src/app/api/models/api-board.model';
   name: 'sort',
 })
 export class SortPipe implements PipeTransform {
-  transform(data: ICreateBoardResp[] | [] | null, args: string): ICreateBoardResp[] | [] | null {
-    // const sortDirection = args;
-    // let multiplier = 1;
-
-    // if (sortDirection === 'desc') {
-    //   multiplier = -1;
-    // }
-
-    if (!data || data.length === 0) {
-      return data;
+  transform(boards: ICreateBoardResp[] | [] | null, args: string): ICreateBoardResp[] | [] | null {
+    if (!boards || boards.length === 0) {
+      return boards;
     }
 
-    // data.sort((a, b) => {
-    //   if (a.title.toLowerCase() < b.title.toLowerCase()) {
-    //     return -1 * multiplier;
-    //   }
-    //   if (a.title.toLowerCase() > b.title.toLowerCase()) {
-    //     return 1 * multiplier;
-    //   }
-    //   return 0;
-    // });
+    const data = [...boards];
+
     return data.sort((a, b) => {
       if (args === 'asc') {
         return a.title > b.title ? 1 : -1;
