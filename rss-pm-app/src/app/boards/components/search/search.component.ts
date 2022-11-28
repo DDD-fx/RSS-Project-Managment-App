62262a61e529028009a22b06e981b31a5cba6600
alexsmirnova13 <alexandrasmirnovae@gmail.com>
2022-11-28 18:28:38 +0300

    fix: css

--- rss-pm-app/src/app/boards/components/search/search.component.ts
+++ rss-pm-app/src/app/boards/components/search/search.component.ts
@@ -26,7 +26,6 @@ export class SearchComponent implements OnInit {
     this.dialogRef.open(PopupSearchResultsComponent, {
       data: { searchText: this.searchInput.get(['search'])?.value },
       panelClass: 'customSearch',
-      backdropClass: 'customSearchBackdrop',
     });
   }
 }
